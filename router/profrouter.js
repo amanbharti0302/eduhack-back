219da2dcc2c8c64784ff769f3be8f395d06960a7
amanbharti0302 <amanbharti0302@gmail.com>
2021-01-17 00:52:28 +0530

    completed professor routes

--- router/profrouter.js
+++ router/profrouter.js
@@ -4,11 +4,27 @@ const router = express.Router();
 const multer = require('multer');
 const professor = require('../schema/profschema');
 
+const multerFilter = (req, file, cb) => {
+      if (file.mimetype.startsWith('application/pdf')) {
+        file.filename = file.originalname;
+        cb(null, true);
+  } else {
+    cb(('Not an image! Please upload only images.', 400), false);
+  }
+};
 
-router.post('/signup', profcontroller.signup);
-router.post('/login', profcontroller.login);
-router.post('/new', profcontroller.newprofessor);
+const upload = multer({
+    fileFilter: multerFilter,
+    dest:'filehandle/'
+});
 
-router.post('/getteacher', profcontroller.getteacher);
+router.post('/signup',profcontroller.signup);
+router.post('/login',profcontroller.login);
+router.post('/new',profcontroller.newprofessor);
+router.post('/assigncourse',profcontroller.assigncourse);
+router.post('/newassignment',profcontroller.newassignment);
+router.post('/studentreport',upload.any('pdf'),profcontroller.getstudentreport);
+router.post('/getteacher',profcontroller.getteacher);
+router.post('/givemarks',profcontroller.givemarks);
 
 module.exports = router;
